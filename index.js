06e5131f35bb03a39f0ba311a76e965c56a9d49f
Luca Ramirez <lukitaz.ramirezjajaja@gmail.com>
2025-07-17 16:44:36 -0300

    + ADD | Estructuración

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const config = require('./config/config.json')
+const fs = require('fs');
+require('colors')
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.GuildExpressions,
+    ],
+    partials: [Discord.Partials.User, Discord.Partials.Channel, Discord.Partials.GuildMember, Discord.Partials.Message, Discord.Partials.Reaction]
+})
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.color = config.color;
+
+Object.freeze(client.la);
+
+fs.readdirSync('./handlers').forEach(handler => {
+    try {
+        require(`./handlers/${handler}`)(client, Discord);
+    } catch (e) {
+        console.log(`ERROR EN EL HANDLER ${handler}`.red)
+        console.log(e)
+    }
+});
+
+client.login(config.token).catch(() => console.log(`-[X]- NO HAS ESPECIFICADO UN TOKEN VALIDO O TE FALTAN INTENTOS -[X]-\n [-] ACTIVA LOS INTENTOS EN https://discord.dev [-]`.red))
